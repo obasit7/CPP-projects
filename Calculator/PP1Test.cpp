541103143e6c5eee5e2448081edaeede484c6f57
obasit7 <87975115+obasit7@users.noreply.github.com>
2022-04-09 07:26:16 -0400

    Update PP1Test.cpp

--- Calculator/PP1Test.cpp
+++ Calculator/PP1Test.cpp
@@ -4,9 +4,6 @@
 // How to compile: g++ -std=c++17 -Wall -I$(CATCH_SINGLE_INCLUDE) (All cpp files)
 // Example if Catch2 and source files are in this directory and at directory level: 
 //    Example: g++ -std=c++17 -Wall *.cpp
-// To see what tests were successful and failed, run your executable with the -s flag
-//    Example: a.out -s
-// A successful test should output: All tests passed (22 assertions in 1 test case)
 /**************************************************************************************************/
 
 #define CATCH_CONFIG_MAIN
